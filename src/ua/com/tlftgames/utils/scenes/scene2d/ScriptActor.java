405614f03e2c4669a2f13fd18b308e9509dc2fde
chibisl <chibisl@ukr.net>
2018-02-05 22:37:10 +0200

    + add new methods for scripts

--- src/ua/com/tlftgames/utils/scenes/scene2d/ScriptActor.java
+++ src/ua/com/tlftgames/utils/scenes/scene2d/ScriptActor.java
@@ -1,35 +1,49 @@
 package ua.com.tlftgames.utils.scenes.scene2d;
 
+import java.util.Collection;
+import java.util.HashMap;
+
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.utils.Array;
 
 import ua.com.tlftgames.utils.scenes.scene2d.script.Script;
 import ua.com.tlftgames.utils.scenes.scene2d.script.ScriptEvent;
 
 public class ScriptActor extends Actor {
-    private Array<Script> scripts = new Array<Script>();
+    private HashMap<Class<? extends Script>, Script> scripts = new HashMap<Class<? extends Script>, Script>();
 
-    public void addScript(Script script) {
+    public <T extends Script> T getScript(Class<T> type) {
+        return type.cast(scripts.get(type));
+    }
+
+    public <T extends Script> boolean hasScript(Class<T> type) {
+        return scripts.get(type) != null;
+    }
+
+    public <T extends Script> void addScript(T script) {
         script.setActor(this);
-        this.scripts.add(script);
+        scripts.put(script.getClass(), script);
+    }
+
+    public <T extends Script> void removeScript(Class<T> type) {
+        scripts.remove(type);
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        Array<Script> scripts = this.scripts;
-        if (scripts.size > 0) {
-            for (int i = 0; i < scripts.size; i++) {
-                scripts.get(i).act(delta);
+        Collection<Script> scripts = this.scripts.values();
+        if (scripts.size() > 0) {
+            for (Script script : scripts) {
+                script.act(delta);
             }
         }
     }
 
     public void dispatch(ScriptEvent event) {
-        Array<Script> scripts = this.scripts;
-        if (scripts.size > 0) {
-            for (int i = 0; i < scripts.size; i++) {
-                scripts.get(i).handle(event);
+        Collection<Script> scripts = this.scripts.values();
+        if (scripts.size() > 0) {
+            for (Script script : scripts) {
+                script.handle(event);
             }
         }
     }
